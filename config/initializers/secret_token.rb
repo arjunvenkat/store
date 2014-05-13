2498e3a5e13b85ba9373f7cbe61d8b79c8fd4e39
Arjun Venkataswamy <you@example.com>
2014-05-13 23:28:50 +0000

    adds model scaffolds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '021d5c1a2221b13de370217354a32046f948a2c23e3aa2d2c4eb313d4f8770aefe287dc4bef232c89c2df713b99dd9f2fecbb943c18b3998240a06abc262d9e8'
